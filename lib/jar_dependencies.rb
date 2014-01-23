d1660bc9b1167fbdf79f351c709c7f29d2da3c38
Christian Meier <m.kristian@web.de>
2014-01-23 10:17:38 +0000

    round it up

--- lib/jar_dependencies.rb
+++ lib/jar_dependencies.rb
@@ -1,4 +1,10 @@
 def require_jar( groupId, artifactId, version, classifier = nil )
+  name = "#{groupId}/#{artifactId}-#{version}"
+  name += "-#{classifier}" if classifier
+  require_jarfile( "#{name}.jar", groupId, artifactId, version, classifier )
+end
+
+def require_jarfile( file, groupId, artifactId, version, classifier = nil )
   skip = java.lang.System.get_property( 'jruby.skip.jars' ) || ENV[ 'JRUBY_SKIP_JARS' ]
   return false if skip == 'true'
   @@jars ||= {}
@@ -10,13 +16,16 @@ def require_jar( groupId, artifactId, version, classifier = nil )
     end
     false
   else
-    name = "#{groupId}/#{artifactId}-#{version}"
-    name += "-#{classifier}" if classifier
-    if require "#{name}.jar"
-      @@jars[ coordinate ] = version
-      true
-    else
-      raise LoadError.new( "coordindate #{coordinate} not found" )
+    begin
+      if require file
+        @@jars[ coordinate ] = version
+        true
+      else
+        raise LoadError.new( "coordindate #{coordinate} not found" )
+      end
+    rescue LoadError => e
+      warn 'you might need to restall that gem which needs the missing jar'
+      raise e
     end
   end
 end
--- simple/simple.gemspec
+++ simple/simple.gemspec
@@ -2,12 +2,19 @@
 
 Gem::Specification.new do |s|
   s.name = 'simple'
-  s.version = "1"
+  s.version = "2"
   s.author = 'sample person'
   s.email = [ 'mail@example.com' ]
   s.summary = 'gem with jar'
   s.description = 'gem with empty jar and jar dependencies'
+  s.extensions << 'Rakefile'
+  s.files << Dir[ 'lib/**/*.rb' ]
+  s.files << Dir[ 'lib/**/*.jar' ]
+  s.files << Dir[ '*file' ]
+  s.files << 'simple.gemspec'
+
   s.add_runtime_dependency 'jar-dependencies', '0.0.1'
+
   s.requirements << "jar org.bouncycastle:bcpkix-jdk15on, 1.49"
   s.requirements << "jar org.bouncycastle:bcprov-jdk15on, 1.49"
 end
